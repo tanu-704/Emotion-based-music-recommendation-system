4d270156379e1b2493e92b3e6b07e53fe27e38fb
TANU DAHIYA <136063925+tanu-704@users.noreply.github.com>
2026-06-14 19:35:16 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,103 +1,159 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
-import cv2 
-import numpy as np 
-import mediapipe as mp 
+import cv2
+import numpy as np
+import mediapipe as mp
 from keras.models import load_model
 import webbrowser
+import os
 
-# model  = load_model("model.h5")
-model = load_model("model.h5", compile = False)
+# ----------------------------
+# Load Model & Labels
+# ----------------------------
+model = load_model("model.h5", compile=False)
 label = np.load("labels.npy")
-holistic = mp.solutions.holistic
-hands = mp.solutions.hands
-holis = holistic.Holistic()
+
+# ----------------------------
+# Mediapipe Setup
+# ----------------------------
+mp_holistic = mp.solutions.holistic
+mp_hands = mp.solutions.hands
 drawing = mp.solutions.drawing_utils
 
-st.header("Emotion Based Music Recommender")
+holis = mp_holistic.Holistic(
+    static_image_mode=False,
+    model_complexity=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+# ----------------------------
+# Streamlit UI
+# ----------------------------
+st.header("🎵 Emotion Based Music Recommender")
 
 if "run" not in st.session_state:
-	st.session_state["run"] = "true"
+    st.session_state["run"] = True
 
-try:
-	emotion = np.load("emotion.npy")[0]
-except:
-	emotion=""
+# ----------------------------
+# Load Emotion Safely
+# ----------------------------
+emotion = ""
+if os.path.exists("emotion.npy"):
+    try:
+        emotion = np.load("emotion.npy", allow_pickle=True)[0]
+    except:
+        emotion = ""
 
-if not(emotion):
-	st.session_state["run"] = "true"
+if not emotion:
+    st.session_state["run"] = True
 else:
-	st.session_state["run"] = "false"
+    st.session_state["run"] = False
 
+# ----------------------------
+# Emotion Processor Class
+# ----------------------------
 class EmotionProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
 
-		##############################
-		frm = cv2.flip(frm, 1)
+        frm = cv2.flip(frm, 1)
+        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
 
-		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        lst = []
 
-		lst = []
+        # ---------------- FACE ----------------
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
+        else:
+            lst.extend([0.0] * 468 * 2)
 
-		if res.face_landmarks:
-			for i in res.face_landmarks.landmark:
-				lst.append(i.x - res.face_landmarks.landmark[1].x)
-				lst.append(i.y - res.face_landmarks.landmark[1].y)
+        # ---------------- LEFT HAND ----------------
+        if res.left_hand_landmarks:
+            for i in res.left_hand_landmarks.landmark:
+                lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+        else:
+            lst.extend([0.0] * 21 * 2)
 
-			if res.left_hand_landmarks:
-				for i in res.left_hand_landmarks.landmark:
-					lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-					lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-			else:
-				for i in range(42):
-					lst.append(0.0)
+        # ---------------- RIGHT HAND ----------------
+        if res.right_hand_landmarks:
+            for i in res.right_hand_landmarks.landmark:
+                lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+        else:
+            lst.extend([0.0] * 21 * 2)
 
-			if res.right_hand_landmarks:
-				for i in res.right_hand_landmarks.landmark:
-					lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-					lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-			else:
-				for i in range(42):
-					lst.append(0.0)
+        lst = np.array(lst).reshape(1, -1)
 
-			lst = np.array(lst).reshape(1,-1)
+        # ---------------- PREDICTION ----------------
+        pred = label[np.argmax(model.predict(lst, verbose=0))]
 
-			pred = label[np.argmax(model.predict(lst))]
+        # Save emotion
+        np.save("emotion.npy", np.array([pred]))
 
-			print(pred)
-			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+        # Show text on frame
+        cv2.putText(frm, str(pred), (50, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 0, 0), 2)
 
-			np.save("emotion.npy", np.array([pred]))
+        # ---------------- DRAW LANDMARKS ----------------
+        if res.face_landmarks:
+            drawing.draw_landmarks(
+                frm,
+                res.face_landmarks,
+                mp_holistic.FACEMESH_TESSELATION,
+                drawing.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1),
+                drawing.DrawingSpec(thickness=1)
+            )
 
-			
-		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
-								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
-								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
-		drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
-		drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+        if res.left_hand_landmarks:
+            drawing.draw_landmarks(frm, res.left_hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        if res.right_hand_landmarks:
+            drawing.draw_landmarks(frm, res.right_hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
 
 
-		##############################
-
-		return av.VideoFrame.from_ndarray(frm, format="bgr24")
-
+# ----------------------------
+# Inputs
+# ----------------------------
 lang = st.text_input("Language")
-singer = st.text_input("singer")
+singer = st.text_input("Singer")
 
-if lang and singer and st.session_state["run"] != "false":
-	webrtc_streamer(key="key", desired_playing_state=True,
-				video_processor_factory=EmotionProcessor)
+# ----------------------------
+# Start Camera
+# ----------------------------
+if lang and singer and st.session_state["run"]:
+    webrtc_streamer(
+        key="emotion-app",
+        desired_playing_state=True,
+        video_processor_factory=EmotionProcessor
+    )
 
-btn = st.button("Recommend me songs")
+# ----------------------------
+# Recommendation Button
+# ----------------------------
+btn = st.button("Recommend Me Songs 🎶")
 
 if btn:
-	if not(emotion):
-		st.warning("Please let me capture your emotion first")
-		st.session_state["run"] = "true"
-	else:
-		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
-		np.save("emotion.npy", np.array([""]))
-		st.session_state["run"] = "false"
-#
+    if not emotion:
+        st.warning("Please allow camera to detect your emotion first.")
+        st.session_state["run"] = True
+    else:
+        query = f"{lang}+{emotion}+song+{singer}"
+        url = f"https://www.youtube.com/results?search_query={query}"
+
+        st.success("Opening YouTube recommendations...")
+        st.markdown(f"[👉 Click here if not opened]({url})")
+
+        # Optional fallback
+        webbrowser.open(url)
+
+        np.save("emotion.npy", np.array([""]))
+        st.session_state["run"] = False
+		
